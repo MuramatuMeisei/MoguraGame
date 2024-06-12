34e9d0907dc10f3535e2444400a9f16cb616c73e
sheepMEEMEE <meemee11033@gmail.com>
2024-06-12 12:13:18 +0900

    モグラをランダムにする(途中)

--- Assets/C#/GameController.cs
+++ Assets/C#/GameController.cs
@@ -5,41 +5,57 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    private int points = 0;
+    public float moveSpeed = 2.0f;
+    public float amplitude = 1.0f;
+    private float positionY;
+    private bool hit = false;
 
     public int pointValue = 10;
     public Text textPoint;
+    private int points = 0;
 
     private void Start()
     {
         UpdatePointsText();
+        positionY = transform.position.y;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //モグラの動き
+        float yOffSet = Mathf.Sin(Time.time * moveSpeed) * amplitude;
+        Vector3 newPosition = transform.position;
+        newPosition.y = positionY + yOffSet;
+        transform.position = newPosition;
+
+         // Targetの物だけ反応
+        if (Input.GetMouseButtonDown(0) && !hit) // モグラが叩かれていない場合にのみ反応
         {
             Vector3 mousePos = Input.mousePosition;
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
-            RaycastHit hit;
+            RaycastHit hitInfo;
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                if (hit.collider.CompareTag("Target"))
+                if (hitInfo.collider.CompareTag("Target"))
                 {
+                    // モグラを叩いたらスコアを加算し、モグラが引っ込むまで再度叩けないようにする
                     AddPoints(pointValue);
+                    hit = true; // モグラが叩かれたことを示すフラグを立てる
                 }
             }
         }
     }
 
+    //点数を加算
     private void AddPoints(int amount)
     {
         points += amount;
         UpdatePointsText();
     }
-
+    
+    //UIにScoreを表示、UIの数字を更新
     private void UpdatePointsText()
     {
         if(textPoint != null)
